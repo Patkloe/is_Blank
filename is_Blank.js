0a1159b96952c0279f52a24d4b1bc8b8312d3a5b
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-02-22 07:05:08 -0600

    Create is_Blank.js

---
+++ is_Blank.js
@@ -0,0 +1,11 @@
+function is_Blank(s){
+
+ if(s.length === 0)   // if the length property of the string is equal to 0, then the string is empty
+
+    return true;   // empty we return true
+
+ return false;    // otherwise false
+
+}
+is_Blank('');
+is_Blank('abc');
